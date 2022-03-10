2ac842132f28b7a889b7077ad5118766b59a59b7
ewizzy <50742104+ewizzy@users.noreply.github.com>
2022-03-10 13:22:30 +0100

    responsive fix

--- components/result.js
+++ components/result.js
@@ -10,19 +10,19 @@ const Result = ({ date, team1, team2, result, stadium, team1img, team2img, team1
 		<div className={styles.resultWrap}>
 		<Container>
 		<Row>
-		<Col md="2" lg="2" className={styles.dateWrap}>
+		<Col md="2" lg="2" sm="12" xs="12" className={styles.dateWrap}>
 		<img src="./calendar.svg" alt="calendar icon" /> {date}
 		</Col>
-		<Col md="3" lg="3" className={styles.team1Wrap}>
+		<Col md="3" lg="3" sm="5" xs="5" className={styles.team1Wrap}>
 		<Link href={homeTeamLink}><a href={homeTeamLink}><img src={team1img} className={styles.teamIcon} alt="team icon" /> {team1}</a></Link>
 		</Col>
-		<Col md="1" lg="1" className={styles.scoreWrap}>
+		<Col md="1" lg="1" sm="2" xs="2" className={styles.scoreWrap}>
 		{result}
 		</Col>
-		<Col md="3" lg="3" className={styles.team2Wrap}>
+		<Col md="3" lg="3" sm="5" xs="5" className={styles.team2Wrap}>
 		<Link href={awayTeamLink}><a href={awayTeamLink}>{team2} <img src={team2img} className={styles.teamIcon} alt="team icon" /></a></Link>
 		</Col>
-		<Col md="3" lg="3" className={styles.stadiumWrap}>
+		<Col md="3" lg="3" sm="12" xs="12" className={styles.stadiumWrap}>
 		<img src="./stadium.png" alt="stadium icon" /> {stadium}
 		</Col>
 		</Row>
